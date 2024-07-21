9f2127c8626eb918429b6cec488cf1aa4c3d8eb5
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-07-21 12:22:10 -0400

    "Updated package-lock.json, package.json, and src files to include new dependencies and tests for CitySearch component"

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,33 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import CitySearch from './components/Citysearch';
+
+describe('<CitySearch /> component', () => {
+    let CitySearchComponent;
+    beforeEach(() => {
+        CitySearchComponent = render(<CitySearch />);
+    });
+    test('renders text input', () => {
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        expect(cityTextBox).toBeInTheDocument();
+        expect(cityTextBox).toHaveClass('city');
+    });
+
+    test('suggestions list is hidden by default', () => {
+        const suggestionList = CitySearchComponent.queryByRole('list');
+        expect(suggestionList).not.toBeInTheDocument();
+    });
+
+    test('renders a list of suggestions when city textbox gains focus', async () => {
+        const user = userEvent.setup();
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.click(cityTextBox);
+        const suggestionList = CitySearchComponent.queryByRole('list');
+        expect(suggestionList).toBeInTheDocument();
+        expect(suggestionList).toHaveClass('suggestions');
+    });
+});
