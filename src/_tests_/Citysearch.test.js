341b029afcbd7a58b2df538362e9af8021a5b7c6
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-07-21 13:05:04 -0400

    "Removed unnecessary `rerender` and `CitySearchComponent` references in CitySearch.test.js"

--- src/_tests_/Citysearch.test.js
+++ src/_tests_/Citysearch.test.js
@@ -1,4 +1,3 @@
-
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import CitySearch from '../components/CitySearch';
@@ -32,39 +31,35 @@ describe('<CitySearch /> component', () => {
         const user = userEvent.setup();
         const allEvents = await getEvents();
         const allLocations = extractLocations(allEvents);
-        const { queryByRole, queryAllByRole, rerender } = render(<CitySearch allLocations={allLocations} />);
+        const { queryByRole, queryAllByRole } = render(<CitySearch allLocations={allLocations} />);
 
-        // user types "Berlin" in city textbox
         const cityTextBox = queryByRole('textbox');
         await user.type(cityTextBox, "Berlin");
 
-        // filter allLocations to locations matching "Berlin"
         const suggestions = allLocations ? allLocations.filter((location) => {
             return location.toUpperCase().indexOf(cityTextBox.value.toUpperCase()) > -1;
         }) : [];
 
-        // get all <li> elements inside the suggestion list
         const suggestionListItems = queryAllByRole('listitem');
         expect(suggestionListItems).toHaveLength(suggestions.length + 1);
         for (let i = 0; i < suggestions.length; i += 1) {
             expect(suggestionListItems[i].textContent).toBe(suggestions[i]);
         }
     });
-});
 
-test('renders the suggestion text in the textbox upon clicking on the suggestion', async () => {
-    const user = userEvent.setup();
-    const allEvents = await getEvents();
-    const allLocations = extractLocations(allEvents);
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    test('renders the suggestion text in the textbox upon clicking on the suggestion', async () => {
+        const user = userEvent.setup();
+        const allEvents = await getEvents();
+        const allLocations = extractLocations(allEvents);
+        const { queryByRole, queryAllByRole } = render(<CitySearch allLocations={allLocations} />);
 
-    const cityTextBox = CitySearchComponent.queryByRole('textbox');
-    await user.type(cityTextBox, "Berlin");
+        const cityTextBox = queryByRole('textbox');
+        await user.type(cityTextBox, "Berlin");
 
-    // the suggestion's textContent look like this: "Berlin, Germany"
-    const BerlinGermanySuggestion = CitySearchComponent.queryAllByRole('listitem')[0];
+        const BerlinGermanySuggestion = queryAllByRole('listitem')[0];
 
-    await user.click(BerlinGermanySuggestion);
+        await user.click(BerlinGermanySuggestion);
 
-    expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
+        expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
+    });
 });
