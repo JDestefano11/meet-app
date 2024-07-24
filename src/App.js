bd4a5808d201596aa747531d566ee71b80e73174
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-07-24 13:33:16 -0400

    "Added useEffect and fetchData function to App.js, updated EventList component to receive events as prop, and added new test for EventList integration"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
+import { getEvents } from './api';
+
+
+
 
 const App = () => {
   const [numberOfEvents, setNumberOfEvents] = useState(32);
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+
 
   return (
     <div className="App">
       <CitySearch />
-      <NumberOfEvents setNumberOfEvents={setNumberOfEvents} />
-      <EventList />
+      <NumberOfEvents />
+      <EventList events={events} />
     </div>
   );
 };
--- src/_tests_/EventList.test.js
+++ src/_tests_/EventList.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, within, waitFor } from '@testing-library/react';
 import EventList from '../components/EventList';
 import { getEvents } from '../api';
+import App from '../App';
 
 describe('<EventList /> component', () => {
     let EventListComponent;
@@ -11,8 +12,7 @@ describe('<EventList /> component', () => {
     });
 
     test('has an element with "list" role', () => {
-        const { queryByRole } = EventListComponent;
-        expect(queryByRole("list")).toBeInTheDocument();
+        expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
     });
 
     test('renders correct number of events', async () => {
@@ -20,6 +20,16 @@ describe('<EventList /> component', () => {
         EventListComponent.rerender(<EventList events={allEvents} />);
         expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
     });
-
 });
 
+describe('<EventList /> integration', () => {
+    test('renders a list of 32 events when the app is mounted and rendered', async () => {
+        const AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
+        const EventListDOM = AppDOM.querySelector('#event-list');
+        await waitFor(() => {
+            const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+            expect(EventListItems.length).toBe(32);
+        });
+    });
+});
