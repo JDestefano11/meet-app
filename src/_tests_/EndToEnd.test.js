cc0b2f3ace5b9d9b7b4434dc600c710f92242251
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-07-24 21:47:45 -0400

    "Refactor EndToEnd.test.js: update test suite structure, add new tests, and modify existing test logic"

--- src/_tests_/EndToEnd.test.js
+++ src/_tests_/EndToEnd.test.js
@@ -1,25 +1,53 @@
 import puppeteer from 'puppeteer';
 
-jest.setTimeout(30000);
-
-describe('End-to-end tests', () => {
+describe('show/hide event details', () => {
     let browser;
     let page;
 
+    // beforeAll(async () => {
+    //     browser = await puppeteer.launch({ headless: true });
+    //     page = await browser.newPage();
+    //     await page.goto('http://localhost:3000/');
+    //     await page.waitForSelector('li', { timeout: 5000 });
+    // });
+
     beforeAll(async () => {
-        browser = await puppeteer.launch();
+        browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 250, // slows down Puppeteer operations by 250ms
+            args: ['--start-maximized'] // starts the browser window maximized
+        });
         page = await browser.newPage();
+        await page.setViewport({ width: 1366, height: 768 });
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('li', { timeout: 5000 });
     });
+
 
     afterAll(async () => {
         await browser.close();
     });
 
     test('An event element is collapsed by default', async () => {
-        await page.goto('http://localhost:3000/');
-        await page.waitForSelector('li', { timeout: 10000 });
+        const eventDetails = await page.$('[data-testid="event-details"]');
+        expect(eventDetails).toBeNull();
+    });
 
+    test('User can expand an event to see details', async () => {
+        await page.click('li button');
+        const eventDetails = await page.$('[data-testid="event-details"]');
+        expect(eventDetails).not.toBeNull();
+    });
+
+    test('User can collapse an event to hide details', async () => {
+        await page.click('li button');
+        await page.waitForSelector('[data-testid="event-details"]', { hidden: true });
         const eventDetails = await page.$('[data-testid="event-details"]');
         expect(eventDetails).toBeNull();
     });
 });
+
+
+
+
+
