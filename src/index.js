8a7c71ee00068033d4599d4107633de5dc14ad27
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-07-30 16:41:04 -0400

    "Updated index.html and index.js files with minor formatting changes and added Atatus import and configuration."

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+import * as atatus from "atatus-spa";
 
-import * as atatus from 'atatus-spa';
-atatus.config('5f92d7964c2649c192116e9e976b675e').install();
+atatus.config("5f92d7964c2649c192116e9e976b675e").install();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -24,4 +24,3 @@ serviceWorkerRegistration.register();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
